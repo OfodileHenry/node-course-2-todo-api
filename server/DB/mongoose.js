86eb06df102b0ac9c09aac9dfa6e23194fc4bc7d
Henry <ofodilechukwuka@gmail.com>
2018-11-22 12:05:52 +0100

    m

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,7 +1,5 @@
 var mongoose=require("mongoose");
-module.exports={
-  mongoose
-}
+module.exports={mongoose};
 
 mongoose.Promise=global.Promise;
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/TodoApp");
